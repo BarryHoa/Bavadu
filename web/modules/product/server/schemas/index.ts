87adc355bdd62fc0f9d9744e9259b136d75ab879
hoatd@atalink.vn <hoatd@atalink.vn>
2025-10-31 16:49:49 +0700

    feat: remove product Modeal

--- web/modules/product/server/schemas/index.ts
+++ web/modules/product/server/schemas/index.ts
@@ -1,7 +1,6 @@
 // New Schemas (UUID-based)
 export * from "./product-master";
 export * from "./product-variant";
-export * from "./product-variant-image";
 export * from "./product-packing";
 export * from "./product-attribute";
 export * from "./product-category";
